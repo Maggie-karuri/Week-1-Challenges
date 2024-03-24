6a1146741c507547945909626c31fd42acbd6258
Maggie-karuri <karurimargaret111@gmail.com>
2024-03-24 18:59:25 +0300

    create student calculator

---
+++ index.js
@@ -0,0 +1,25 @@
+const promptMessage = "Enter marks: ";
+let marksEntered = prompt(promptMessage);
+let marks = parseFloat(marksEntered);
+let grade = studentGrade(marks);
+
+function studentGrade(marks) {
+    if (marks > 79 && marks <= 100) {
+        return 'A';
+    } else if (marks >= 60 && marks <= 79) {
+        return 'B';
+    } else if (marks >= 50 && marks <= 59) {
+        return 'C';
+    } else if (marks >= 40 && marks <= 49) {
+        return 'D';
+    } else if (marks >= 0 && marks <= 39) {
+        return 'E';
+    } else if (marks < 0 || marks > 100) {
+        return 'Invalid!';
+    }
+}
+
+function calculateGrade() {
+    const gradeResult = document.getElementById('gradeResult');
+    gradeResult.textContent = `This student's grade is: ${grade}`;
+}
