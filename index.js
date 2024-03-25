eeba9e8ec8b789cc26366c729cc22c599d3affef
Maggie-karuri <109177261+Maggie-karuri@users.noreply.github.com>
2024-03-25 19:19:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-function speedDetector(speed) {
-    const allowedSpeedLimit = 70;
-    const speedingDemeritRange = 5;
-    const demeritPoints = 1;
-    const speedResults = document.getElementById('speedResults');
-    
-    if (speed <= allowedSpeedLimit) {
-        return("Ok");
-    }
-    else {
-        const speedingRange = speed - allowedSpeedLimit;
-        const accumulatedDemerit = Math.floor(speedingRange / speedingDemeritRange);
-        if (accumulatedDemerit > 12) {
-            return(`You have: ${accumulatedDemerit} points\nLicense Suspended!`)
-        } 
-        else {
-            return("Demerit points: " + accumulatedDemerit);
-        }
-        
-    }
-}
-function calculateSpeed() {
-    const speedEntered = document.getElementById('speedInput').value; 
-    const speed = parseFloat(speedEntered);
-
-    speedResults.textContent = speedDetector(speed);
-=======
 const promptMessage = "Enter marks: ";
 let marksEntered = prompt(promptMessage);
 let marks = parseFloat(marksEntered);
